3b0ad6a28f91cd4f1e30b28b6130f4ef37a0f8d0
Kenji_Chao <kenji_chao@asus.com>
2016-04-15 19:29:09 +0800

    2015 Round 1B P3 small 2

--- 2015/Round_1B/HikingDeer.java
+++ 2015/Round_1B/HikingDeer.java
@@ -5,7 +5,7 @@ public class HikingDeer {
     private static InputReader in;
     private static PrintWriter out;
     public static boolean SUBMIT = false;
-    public static final String NAME = "C-small-practice-1";
+    public static final String NAME = "C-large-practice";
     private static ArrayList<Human> human;
 
     public static void main(String[] args) throws IOException {
@@ -26,10 +26,12 @@ public class HikingDeer {
             // read input
             human = new ArrayList<>();
             int groups = in.nextInt();
+            // out.println("groups=" + groups);
             for (int i = 0; i < groups; i++) {
                 long start = in.nextLong();
                 int people = in.nextInt();
                 long pace = in.nextLong();
+                // if (i == 0) out.println(start + " " + people + " " + pace);
                 for (int j = 0; j < people; j++, pace++) {
                     human.add(new Human(start, pace));
                 }
@@ -44,20 +46,85 @@ public class HikingDeer {
 
     private static int solve() {
         if (human.size() == 1 || isTheSamePace()) return 0;
+        int len = human.size();
+        long[] firstTimeOnOrigin = new long[len];
+        long[] pace = new long[len];
+        PriorityQueue<Long> queue = new PriorityQueue<>();
+        int ret = len - 1;
 
-        Human human1 = human.get(0);
-        Human human2 = human.get(1);
-
-        // double firstTimeOnOrigin = Math.max((double) human1.start / (double) human1.pace, (double) human2.start / (double) human2.pace);
-        // double secondTimeOnOrigin = Math.min((double) (360 + human1.start) / (double) human1.pace, (double) (360 + human2.start) / (double) human2.pace);
-        long firstTimeOnOrigin = Math.max((360 - human1.start) * human1.pace, (360 - human2.start) * human2.pace);
-        long secondTimeOnOrigin = Math.min((720 - human1.start) *  human1.pace, (720 - human2.start) * human2.pace);
-
-        if (firstTimeOnOrigin < secondTimeOnOrigin) {
-            return 0;
+        for (int i = 0; i < len; i++) {
+            Human h = human.get(i);
+            firstTimeOnOrigin[i] = (360 - h.start) * h.pace;
+            pace[i] = firstTimeOnOrigin[i];
         }
 
-        return 1;
+        ArrayIndexComparator cmp = new ArrayIndexComparator(firstTimeOnOrigin);
+        Integer[] indices = cmp.createIndexArray();
+        Arrays.sort(indices, cmp);
+
+
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                Human h = human.get(j);
+                pace[j] += 360 * h.pace;
+                if (pace[j] <= firstTimeOnOrigin[indices[len - 1]])
+                    queue.add(pace[j]);
+            }
+            if (queue.size() == 0) {
+                ret = Math.min(ret, i - 1);
+                break;
+            }
+
+            long val = queue.poll();
+            int largeElements = binarySearchLargeElements(indices, firstTimeOnOrigin, val);
+            ret = Math.min(ret, largeElements + (i - 1));
+            // if (val > firstTimeOnOrigin[indices[len - 1]]) break;
+        }
+
+        return ret;
+    }
+
+    private static int binarySearchLargeElements(Integer[] indices, long[] value, long target) {
+        int len = indices.length;
+        int low = 0;
+        int high = len;
+        while (low + 1 < high) {
+            int mid = (low + high) / 2;
+            if (value[indices[mid]] < target) {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+
+        return len - high;
+    }
+
+    private static class ArrayIndexComparator implements Comparator<Integer> {
+        long[] nums;
+
+        public ArrayIndexComparator(long[] nums) {
+            this.nums = nums;
+        }
+
+        public Integer[] createIndexArray() {
+            Integer[] indices = new Integer[nums.length];
+            for (int i = 0; i < indices.length; i++) {
+                indices[i] = i;
+            }
+            return indices;
+        }
+
+        @Override
+        public int compare(Integer i1, Integer i2) {
+            if (nums[i1] < nums[i2]) {
+                return -1;
+            } else if (nums[i1] > nums[i2]) {
+                return 1;  
+            } else {
+                return i1.compareTo(i2);
+            }
+        } 
     }
 
     private static boolean isTheSamePace() {
